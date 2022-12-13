c3acc6b78cb9493bb421e1b2ece5aa4c7239153f
Vivek KUMAR <enigmaticvision@gmail.com>
2022-12-13 05:40:15 +0000

    Date 13 Dec 2022

--- 2. Assignment1_Rotation of List.py
+++ 2. Assignment1_Rotation of List.py
@@ -13,13 +13,15 @@ def count_rotations_binary(nums):
     lo = 0
     hi = len(nums) -1
 
-    while lo < hi:
+    while True:
         mid = (hi + lo) //2
         mid_number = nums[mid]
 
         # lst = [ _, 25_, 29, 3]
         # Uncomment the next line for logging the values and fixing errors.
         print("lo:", lo, ", hi:", hi, ", mid:", mid, ", mid_number:", mid_number)
+        #lst = [19, 25, 29, 3, 5, 6, 7, 9, 11, 14]
+       # lo: 2, hi: 3, mid: 2, mid_number: 29
 
         if mid > 0 and mid_number < nums[mid-1] :
             # The middle position is the answer
@@ -42,8 +44,9 @@ def count_rotations_binary(nums):
 # lst = [2, 3, 4 , 5,6 , 9, 10 ,0 ]
 
 
-lst = [19, 25, 29, 3, 5, 6, 7, 9, 11, 14]
-#print(rotationList_Linier(lst))
+lst = [19, 25, 29, 3, 5, 6, 7, 9, 11,14]
+
+print(rotationList_Linier(lst))
 print(count_rotations_binary(lst))
 
 # 2. List has 10 element (Even) , adn rotate 3 time
